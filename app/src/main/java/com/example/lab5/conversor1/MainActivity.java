1fc72f58c52d89b2e522b416f881b9a4646d4e4e
lizzy0015 <lizzaisaula@gmail.com>
2018-12-09 04:15:14 +0000

    programacion de textviw

--- app/src/main/java/com/example/lab5/conversor1/MainActivity.java
+++ app/src/main/java/com/example/lab5/conversor1/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.lab5.conversor1;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button temperatura=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        temperatura=(Button)findViewById(R.id.btn1);
+
+        temperatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cambiar=new Intent(getApplicationContext(),temperatura.class);
+            }
+        });
     }
 }
